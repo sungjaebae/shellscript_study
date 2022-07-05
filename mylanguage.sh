ed4a9b2a0a24b561d50162a298a6effd086e3172
sungjae bae <hsm0156@gmail.com>
2022-07-05 08:37:10 +0000

    220705 화요일 자료

---
+++ mylanguage.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "This shell script name is $0"
+echo "I can speak $1 and $2"
+echo "This shell script parameters are $*"
+echo "this shell script parameters are $@"
+echo "This parameter count is $#"
---
+++ mylanguage4.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+for language in "$*"
+do
+    echo "I can speack $language"
+done
